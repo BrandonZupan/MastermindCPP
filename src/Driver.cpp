706f51931f06163b15bbf2b1aa32cf9c7f617212
brandon <brandon@pop-os.localdomain>
2020-05-17 12:31:16 -0500

    Initial commit

---
+++ src/Driver.cpp
@@ -0,0 +1,13 @@
+// Driver.cpp
+// Driver file for Mastermind
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "hello world" << endl; 
+
+}
+
